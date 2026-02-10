2048cb780fe01c1e0d698a18db5a9e5158e43998
aryavhir <aryavhirkoul1@gmail.com>
2026-02-10 15:33:05 +0530

    done

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -2,7 +2,18 @@ export default [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  'strapi::cors',
+   {
+    name: 'strapi::cors',
+    config: {
+      origin: [
+        'https://ad-mandala-q8cq.vercel.app',
+        'https://www.admandala.com/', // your frontend
+      ],
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+      headers: ['Content-Type', 'Authorization'],
+      credentials: false,
+    },
+  },
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
